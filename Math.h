b5acb1ddf80431586289ffab3ce18c1ca3458479
G1rmmr <blckhnd.std@gmail.com>
2025-12-27 15:41:35 +0900

    add/vector3

--- Math.h
+++ Math.h
@@ -1,7 +1,9 @@
 #pragma once
 
-#include <memory>
-#include <immintrin.h>
+#include <cmath>
+#include <cassert>
+
+#include "SIMD.h"
 
 namespace math{
     struct alignas(16) Vector3{
@@ -13,44 +15,263 @@ namespace math{
                 float W;
             };
 
-            __m128 
+            simd::Floats V;
         };
 
-        Vector3() = default;
-        Vector3(const float x, const float y, const float z) : X(x), Y(y), Z(z){}
+        ~Vector3() noexcept = default;
 
-        Vector3(const Vector3& other) : X(other.X), Y(other.Y), Z(other.Z){}
+        Vector3() noexcept : V(simd::Reset()) {}
 
-        Vector3(Vector3&& other) noexcept : X(0.f), Y(0.f), Z(0.f){ *this = std::move(other); }
+        Vector3(const float val) noexcept : V(simd::Set(val)) {}
 
-        Vector3& operator=(const Vector3& other){
-            X = other.X;
-            Y = other.Y;
-            Z = other.Z;
-        }
+        Vector3(const simd::Floats& v) noexcept : V(v) {}
 
-        Vector3& operator=(Vector3&& other) noexcept{
-            if(this != &other){
-                X = 0.f;
-                Y = 0.f;
-                Z = 0.f;
-                
-                *this = std::move(other);
-            }
-        }
+        Vector3(const float x, const float y, const float z, const float w = 0.f) noexcept 
+            : V(simd::Set(x, y, z, w)){}
 
-        Vector3 operator+(const Vector3& other){ return {X + other.X, Y + other.Y, Z + other.Z}; }
-        Vector3& operator+=(const Vector3& other){
-            X += other.X;
-            Y += other.Y;
-            Z += other.Z;
+        Vector3(const Vector3& other) noexcept : V(other.V){}
+
+        Vector3(Vector3&& other) noexcept : V(other.V) {}
+
+        Vector3& operator=(const Vector3& other) noexcept {
+            if(this != &other) V = other.V;
             return *this;
         }
 
-        Vector3 operator-(const Vector3& other){ return {X - other.X, Y - other.Y, Z - other.Z}; }
-        Vector3 operator*(const float val){ return {val * X, val * Y, val * Z}; }
-        Vector3 operator/(const float val){ return {X / val, Y / val, Z / val}; }
+        Vector3& operator=(Vector3&& other) noexcept{
+            if(this != &other) V = other.V;
+            return *this;
+        }
 
-        float operator*(const Vector3& other){ return X * other.X + Y * other.Y + Z * other.Z; }
+        Vector3& operator+=(const Vector3& other) noexcept {
+            V = simd::Add(V, other.V);
+            return *this;
+        }
+
+        Vector3 operator+(Vector3 other) const noexcept {
+            other += *this;
+            return other;
+        }
+
+        Vector3& operator-=(const Vector3& other) noexcept {
+            V = simd::Sub(V, other.V);
+            return *this;
+        }
+        
+        Vector3 operator-(Vector3 other) const noexcept {
+            Vector3 temp(*this);
+            temp -= other;
+            return temp;
+        }
+
+        Vector3& operator*=(const float val) noexcept {
+            const simd::Floats temp = simd::Set(val);
+            V = simd::Mul(V, temp);
+            return *this;
+        }
+
+        Vector3 operator*(const float val) const noexcept {
+            Vector3 result(*this);
+            result *= val;
+            return result;
+        }
+
+        Vector3& operator/=(const float val) noexcept {
+            assert(val != 0.f && "Division by zero!");
+            *this *= 1 / val;
+            return *this;
+        }
+
+        Vector3 operator/(const float val) const noexcept {
+            Vector3 result(*this);
+            result /= val;
+            return result;
+        }
+
+        Vector3 Inv() const noexcept { return Vector3(simd::Inv(V)); }
+
+        Vector3 Sqrt() const noexcept { return Vector3(simd::Sqrt(V)); }
+
+        float Dot(const Vector3& other) const noexcept {
+            const Vector3 temp{simd::HorizonSum(V, other.V, 0x71)};
+            return temp.X;
+        }
+
+        bool operator==(const Vector3& other) noexcept {
+            const Vector3 diff = *this - other;
+            float dist = diff.Dot(diff); 
+    
+            const float epsilon = 1e-5f;
+            return dist < (epsilon * epsilon);
+        }
+
+        bool operator!=(const Vector3& other) noexcept {
+            const Vector3 diff = *this - other;
+            float dist = diff.Dot(diff); 
+    
+            const float epsilon = 1e-5f;
+            return dist >= (epsilon * epsilon);
+        }
+        
+        Vector3 Cross(const Vector3& other) const noexcept {
+            const std::uint8_t leftMask = _MM_SHUFFLE(3, 0, 2, 1);
+            const std::uint8_t rightMask = _MM_SHUFFLE(3, 1, 0, 2);
+
+            Vector3 left{simd::Mul(
+                simd::Shuffle(V, V, leftMask),
+                simd::Shuffle(other.V, other.V, rightMask)
+            )};
+
+            Vector3 right{simd::Mul(
+                simd::Shuffle(V, V, rightMask),
+                simd::Shuffle(other.V, other.V, leftMask)
+            )};
+
+            return left - right;
+        }
+
+        float Length() const noexcept {
+            float dot = Dot(*this);
+            return std::sqrt(dot);
+        }
+
+        Vector3 Norm() const noexcept { return *this / Length(); }
+    };
+
+    struct alignas(16) Matrix4{
+        union{
+            simd::Floats Cols[4];
+            float Elements[16];
+        };
+
+        inline float* operator[](int index) noexcept {
+            return reinterpret_cast<float*>(&Cols[index]);
+        }
+
+        inline const float* operator[](int index) const noexcept {
+            return reinterpret_cast<const float*>(&Cols[index]);
+        }
+
+        ~Matrix4() noexcept = default;
+
+        Matrix4() noexcept {
+            Cols[0] = simd::Set(1, 0, 0, 0);
+            Cols[1] = simd::Set(0, 1, 0, 0);
+            Cols[2] = simd::Set(0, 0, 1, 0);
+            Cols[3] = simd::Set(0, 0, 0, 1);
+        }
+
+        Matrix4(const float val) noexcept {
+            for(std::size_t i = 0; i < 4; ++i)
+                Cols[i] = simd::Set(val);
+        }
+        
+        Matrix4(const Matrix4& other) noexcept {
+            for(std::size_t i = 0; i < 4; ++i)
+                Cols[i] = other.Cols[i];
+        }
+
+        Matrix4(Matrix4&& other) noexcept {
+            for(std::size_t i = 0; i < 4; ++i)
+                Cols[i] = other.Cols[i];
+        }
+
+        Matrix4& operator=(const Matrix4& other) noexcept {
+            if(this != &other){
+                for(std::size_t i = 0; i < 4; ++i)
+                    Cols[i] = other.Cols[i];
+            }
+            return *this;
+        }
+
+        Matrix4& operator=(Matrix4&& other) noexcept{
+            if(this != &other){
+                for(std::size_t i = 0; i < 4; ++i)
+                    Cols[i] = other.Cols[i];
+            }
+            return *this;
+        }
+
+        Matrix4& operator+=(const Matrix4& other) noexcept {
+            for(std::size_t i = 0; i < 4; ++i)
+                Cols[i] = simd::Add(Cols[i], other.Cols[i]);
+
+            return *this;
+        }
+
+        Matrix4 operator+(Matrix4 other) const noexcept {
+            other += *this;
+            return other;
+        }
+
+        Matrix4& operator-=(const Matrix4& other) noexcept {
+            for(std::size_t i = 0; i < 4; ++i)
+                Cols[i] = simd::Sub(Cols[i], other.Cols[i]);
+
+            return *this;
+        }
+        
+        Matrix4 operator-(Matrix4 other) const noexcept {
+            Matrix4 temp(*this);
+            temp -= other;
+            return temp;
+        }
+
+        Matrix4& operator*=(const float val) noexcept {
+            const simd::Floats temp = simd::Set(val);
+
+            for(std::size_t i = 0; i < 4; ++i)
+                Cols[i] = simd::Mul(Cols[i], temp);
+            
+            return *this;
+        }
+
+        Matrix4 operator*(const float val) const noexcept {
+            Matrix4 result(*this);
+            result *= val;
+            return result;
+        }
+
+        Matrix4& operator*=(const Matrix4& other) noexcept {
+        }
+
+        Matrix4 operator*(const Matrix4 val) const noexcept {
+        }
+
+        Matrix4& operator/=(const float val) noexcept {
+            assert(val != 0.f && "Division by zero!");
+            *this *= 1 / val;
+            return *this;
+        }
+
+        Matrix4 operator/(const float val) const noexcept {
+            Matrix4 result(*this);
+            result /= val;
+            return result;
+        }
+
+        Matrix4 Inv() const noexcept {
+            Matrix4 mat;
+
+            for(std::size_t i = 0; i < 4; ++i)
+                mat.Cols[i] = simd::Inv(Cols[i]);
+
+            return mat; 
+        }
+
+        Matrix4 Sqrt() const noexcept {
+            Matrix4 mat;
+
+            for(std::size_t i = 0; i < 4; ++i)
+                mat.Cols[i] = simd::Sqrt(Cols[i]);
+
+            return mat;
+        }
+
+        bool operator==(const Matrix4& other) noexcept {
+        }
+
+        bool operator!=(const Vector3& other) noexcept {
+        }
     };
 }
--- SIMD.h
+++ SIMD.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+
 namespace simd{
 
 #if defined(__x86_64__) || defined(_M_X64) || defined(__i386__) || defined(_M_IX86)
@@ -18,75 +20,82 @@ namespace simd{
 #error "지원하지 않는 아키텍처입니다."
 #endif
 
-    inline Floats Add(const Floats& lhs, const Floats& rhs){
+// Arithmetics
+    inline Floats Add(const Floats& lhs, const Floats& rhs) noexcept {
 #ifdef ENGINE_SIMD_SSE
         return _mm_add_ps(lhs, rhs);
-
 #elif defined(ENGINE_SIMD_NEON)
-        return vaddq_f32(lhs, rhs);
-
 #endif
     }
 
-    inline Floats Sub(const Floats& lhs, const Floats& rhs){
+    inline Floats Sub(const Floats& lhs, const Floats& rhs) noexcept {
 #ifdef ENGINE_SIMD_SSE
         return _mm_sub_ps(lhs, rhs);
-
 #elif defined(ENGINE_SIMD_NEON)
-        return vsubq_f32(a, b);
-
 #endif
     }
 
-    inline Floats Mul(const Floats& lhs, const Floats& rhs){
+    inline Floats Mul(const Floats& lhs, const Floats& rhs) noexcept {
 #ifdef ENGINE_SIMD_SSE
         return _mm_mul_ps(lhs, rhs);
-
 #elif defined(ENGINE_SIMD_NEON)
-        return vmulq_f32(a, b);
-
 #endif
     }
 
-    inline Floats Div(const Floats& lhs, const Floats& rhs){
+    inline Floats Div(const Floats& lhs, const Floats& rhs) noexcept {
 #ifdef ENGINE_SIMD_SSE
         return _mm_div_ps(lhs, rhs);
-
 #elif defined(ENGINE_SIMD_NEON)
-        return vdivq_f32(a, b);
-
 #endif
     }
 
-    inline Floats Zero() {
+    inline Floats Inv(const Floats& val) noexcept {
+#ifdef ENGINE_SIMD_SSE
+        return _mm_rcp_ps(val);       
+#elif defined(ENGINE_SIMD_NEON)
+#endif
+    }
+
+    inline Floats Sqrt(const Floats& val) noexcept {
+#ifdef ENGINE_SIMD_SSE
+        return _mm_sqrt_ps(val);
+#elif defined(ENGINE_SIMD_NEON)                
+#endif
+    }
+
+    inline Floats HorizonSum(const Floats& lhs, const Floats& rhs, const std::uint8_t mask) noexcept {
+#ifdef ENGINE_SIMD_SSE
+        return _mm_dp_ps(lhs, rhs, mask);
+#elif defined(ENGINE_SIMD_NEON)                
+#endif
+    }
+
+// Logicals
+    inline Floats Reset() noexcept {
 #ifdef ENGINE_SIMD_SSE
         return _mm_setzero_ps();
-
 #elif defined(ENGINE_SIMD_NEON)
-        return vdupq_n_f32(0.0f);
-
 #endif
     }
 
-    inline Floats Load(const float x, const float y, const float z, const float w){
+    inline Floats Set(const float val) noexcept {
+#ifdef ENGINE_SIMD_SSE
+        return _mm_set1_ps(val);
+#elif defined(ENGINE_SIMD_NEON)
+#endif
+    }
+    
+    inline Floats Set(const float x, const float y, const float z, const float w) noexcept {
 #ifdef ENGINE_SIMD_SSE
         return _mm_set_ps(w, z, y, x);
-
 #elif defined(ENGINE_SIMD_NEON)
-        float temp[4] = {x, y, z, w};
-
-        return vld1q_f32(temp);
 #endif
     }
 
-    inline float* Store(const Floats& data){
+    inline Floats Shuffle(const Floats& lhs, const Floats& rhs, const ::std::uint8_t mask) noexcept {
 #ifdef ENGINE_SIMD_SSE
-        return _mm_set_ps(w, z, y, x);
-
+        return _mm_shuffle_ps(lhs, rhs, mask);
 #elif defined(ENGINE_SIMD_NEON)
-        float temp[4] = {x, y, z, w};
-
-        return vld1q_f32(temp);
 #endif
     }
 }
