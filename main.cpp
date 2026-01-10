9d32639b5d320df32eba088a2f7aaf50f4375a17
G1rmmr <blackhand2413@naver.com>
2026-01-10 18:38:53 +0900

    add/window-build-set

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <cstdio>
+﻿#include <cstdio>
 #include <vector>
 
 #include <MiniFB.h>
@@ -8,37 +8,35 @@
 #include "graphics/Rasterizer.hpp"
 
 int main() {
-    const char* WINDOW_TITLE = "Software Rasterizer (NEON/Minifb)";
+    const char* WINDOW_TITLE = "Software Rasterizer";
 
-    struct mfb_window *window = mfb_open_ex(WINDOW_TITLE, world::WIDTH, world::HEIGHT, WF_RESIZABLE);
-    if (!window) {
+    struct mfb_window* window = mfb_open_ex(WINDOW_TITLE, world::WIDTH, world::HEIGHT, WF_RESIZABLE);
+    if(!window) {
         std::fprintf(stderr, "ERROR: Could not open minifb window\n");
         return -1;
     }
 
     graphics::FrameBuffer frame(world::WIDTH, world::HEIGHT);
 
-    world::CreateIcoSphere(1.f, 3);
+    world::CreateIcoSphere(1.f, 2);
 
     float angle = 0.f;
     do {
         frame.Clear(world::COLOR);
 
-        angle += 0.02f;
-        shader::Default shader{
-            world::GetMVP(angle),
-            math::CreateViewport(static_cast<float>(world::WIDTH), static_cast<float>(world::HEIGHT))
-        };
+        angle += 0.01f;
+        shader::Default shader{world::GetMVP(angle), math::CreateViewport(static_cast<float>(world::WIDTH),
+                                                                          static_cast<float>(world::HEIGHT))};
 
-        graphics::Render(frame, shader, world::ModelVertices, world::ModelIndices, graphics::PrimitiveType::Lines);
+        graphics::Render(frame, shader, world::ModelVertices, world::ModelIndices, graphics::PrimitiveType::Triangles);
 
         int state = mfb_update(window, frame.GetColor());
-        if (state < 0) {
+        if(state < 0) {
             window = nullptr;
             break;
         }
 
-    } while (mfb_wait_sync(window));
+    } while(mfb_wait_sync(window));
 
     return 0;
 }
